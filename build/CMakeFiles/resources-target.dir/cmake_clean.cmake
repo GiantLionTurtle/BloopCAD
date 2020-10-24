1fd948f543a1f9567bf0402db02611e54bbf4367
GiantLionTurtle <charle.tanguay@gmail.com>
2020-10-24 09:59:21 -0400

    Gitignore

--- build/CMakeFiles/resources-target.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/resources-target"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/resources-target.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
