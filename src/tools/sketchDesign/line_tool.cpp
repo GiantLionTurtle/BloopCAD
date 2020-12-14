80dcd54abf58f3c81eb6896c0bc178984ab7fa79
GiantLionTurtle <charle.tanguay@gmail.com>
2020-12-14 17:01:32 -0500

    Functionnal ray cast in line_tool

--- src/tools/sketchDesign/line_tool.cpp
+++ src/tools/sketchDesign/line_tool.cpp
@@ -71,26 +71,12 @@ glm::vec3 line_tool::pos_on_plane(glm::vec2 mousePos)
 	if(target) {
 		std::shared_ptr<camera> cam = mEnv->state()->cam; // For ease of writing
 		
-		float cos_half_FOVy = std::cos(M_PI/2 - cam->FOV() / 2.0f);
-		float half_FOVx = std::atan(std::tan(cam->FOV()/2.0f)*cam->aspectRatio().get());
-		float cos_half_FOVx = std::cos(M_PI/2 - half_FOVx);
-		float heightx = sin(M_PI/2-half_FOVx);
-		float heighty = sin(M_PI/2-cam->FOV() / 2.0f);
-		float mouse_cosx = map((float)mousePos.x, 0.0f, (float)mEnv->state()->doc->get_width(), -cos_half_FOVx, cos_half_FOVx);
-		float mouse_cosy = map((float)mousePos.y, (float)mEnv->state()->doc->get_height(), 0.0f, -cos_half_FOVy, cos_half_FOVy);
-
-		glm::vec3 angles(
-			M_PI/2.0f-std::atan2(heightx, mouse_cosx),
-			std::atan2(heighty, mouse_cosy),
-			0.0f);
-		// ray.z = -std::sqrt(1.0f - (ray.x * ray.x + ray.y * ray.y));
-		glm::vec3 ray(0.0f, 0.0f, -1.0f);
-		ray = glm::rotate(glm::angleAxis(angles.y, glm::vec3(1.0f, 0.0f, 0.0f)) * glm::angleAxis(-angles.x, glm::vec3(0.0f, 1.0f, 0.0f)), ray);
-		
-		// Transform the ray in the world
-		// ray = cam->model_inv() * glm::vec4(ray, 1.0f);
-		std::cout<<"ray: "<<glm::to_string(ray)<<"\n";//<<" /w "<<std::atan(height / map((float)mousePos.x, 0.0f, (float)mEnv->state()->doc->get_width(), -cos_half_FOVx, cos_half_FOVx))<<"\n";
-		// Find where the ray intersect
+		float screen_dist = 1.0f / std::tan(cam->FOV() / 2.0f);
+		float half_screenWidth = cam->aspectRatio().get();
+		glm::vec3 pos_on_screen(map((float)mousePos.x, 0.0f, (float)mEnv->state()->doc->get_width(), -half_screenWidth, half_screenWidth),
+								map((float)mousePos.y, (float)mEnv->state()->doc->get_height(), 0.0f, -1.0f, 1.0f), 
+								-screen_dist);
+		glm::vec3 ray = cam->model_inv() * glm::vec4(glm::normalize(pos_on_screen), 0.0f);
 		return target->basePlane()->line_intersection(mEnv->state()->cam->pos(), ray);
 	}
 	LOG_WARNING("No target.");
